cacb96b5278ca8075c1583620a2487d0f1565ffe
augustoda1 <augustoda@al.insper.edu.br>
2026-02-20 13:07:33 -0300

    exs 0 feitos

--- exe0_cppcheck/main.c
+++ exe0_cppcheck/main.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #define NUM_ELEMENTS 5
 
 int multiplyNumbers(int x, int y) {
     int product = x * y;
+    return product;
 }
 
 int main(void) {
-    int a, b = 5; //uninitvar, legacyUninitvar
+    int a = 0;
+    int b = 5;
+
     int result = multiplyNumbers(a, b);
+    (void)result;
 
     int arr[NUM_ELEMENTS];
-    for(int i = 0; i <= NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
+    for (int i = 0; i < NUM_ELEMENTS; i++) {
         arr[i] = i;
     }
 
